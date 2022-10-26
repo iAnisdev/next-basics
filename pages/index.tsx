d39bd3a5fb03a662db19d0aab59d43b17eec3bd8
Anis <anis.rahman@nfq.asia>
2022-10-26 11:07:56 +0700

    testing pre-render

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,14 @@ import type { NextPage } from 'next'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
+
+  const showMessage = () => {
+    alert('hello')
+  }
   return (
     <div className={styles.container}>
       <h1>Home page</h1>
+      <button onClick={showMessage}>click me</button>
     </div>
   )
 }
