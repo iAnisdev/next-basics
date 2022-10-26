0e61566f73adc04b8f089bc48dbe2759a5cf3b08
Anis <anis.rahman@nfq.asia>
2022-10-26 13:16:17 +0700

    GetStaticProps

--- pages/profile/[userId]/index.tsx
+++ pages/profile/[userId]/index.tsx
@@ -1,7 +1,8 @@
 import { useRouter } from "next/router"
+
 export default function UserProfile() {
     const router = useRouter()
     return <>
-        <div>Profile with id <b>{router.query.userId}</b>  page</div>
+        <div>Profile with id <b>{router.query.userId}</b> page</div>
     </>
 }
---
+++ pages/users.tsx
@@ -0,0 +1,55 @@
+import type { NextPage, GetStaticProps } from "next"
+
+interface user {
+  id: number
+  name: string
+  username: string
+  email: string
+  address: {
+    street: string,
+    suite: string,
+    city: string,
+    zipcode: string
+  }
+  geo: {
+    lat: string
+    lng: string
+  }
+  phone: string
+  website: string
+  company: {
+    name: string,
+    catchPhrase: string
+    bs: string
+  }
+}
+
+interface UserProps {
+  users: user[]
+}
+
+const users: NextPage<UserProps> = (props) => {
+  let listofUsers = props.users.map((user: user) => {
+    return <li key={user.id}>{user.username}</li>
+  })
+  return (
+    <div>
+      <ul>
+        {listofUsers}
+      </ul>
+    </div>
+  )
+}
+
+export default users
+
+export const getStaticProps: GetStaticProps<UserProps> = async (context) => {
+  console.log(context)
+  const response = await fetch('https://jsonplaceholder.typicode.com/users')
+  const data = await response.json()
+  return {
+    props: {
+      users: data
+    }
+  }
+}
