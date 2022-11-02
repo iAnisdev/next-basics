9ad49ddfc9ca4860ffa8037708e836680c7b4aec
Anis <anis.rahman@nfq.asia>
2022-11-02 11:03:48 +0700

    getstaticpaths with getstaticprops

--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -1,7 +1,51 @@
+import { GetStaticPaths, GetStaticProps, NextPage } from "next"
+import { NextParsedUrlQuery } from "next/dist/server/request-meta"
 import { useRouter } from "next/router"
-export default function PostIdpage() {
+import styles from './post.module.css'
+
+interface IParams extends NextParsedUrlQuery {
+    id: any
+}
+interface Post {
+    userId: number
+    id: number
+    title: string
+    body: string
+}
+
+
+interface PostProps {
+    post: Post
+}
+
+const PostIdpage: NextPage<PostProps> = ({post}) => {
     const router = useRouter()
-    return <>
-        <div>Post with id <b>{router.query.id}</b>  page</div>
-    </>
+    return <div className={styles.container}>
+        <h3>{post.title}</h3>
+        <br />
+        <p>{post.body}</p>
+    </div>
+}
+
+export default PostIdpage
+
+export const getStaticPaths: GetStaticPaths = async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+    const data = await response.json()
+    const paths = data.map((post: Post) => ({
+        params: { id: post.id.toString() },
+    }))
+    return { paths, fallback: true }
+}
+
+export const getStaticProps: GetStaticProps<PostProps> = async (context) => {
+    console.log(context.params)
+    const { id } = context.params as IParams
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+    const data = await response.json()
+    return {
+        props: {
+            post: data
+        }
+    }
 }
